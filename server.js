8589d91188b2b7fb8b96b1de0cd6c26b052131a3
pdy1207 <pdyme1207@gmail.com>
2023-09-02 13:09:52 +0900

    fix: 해당 파일 경로 수정 임시 임시 디자인으로서 파일 경로 수정

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const app = express();
+const path = require("path"); // 파일 폴더 가져오기
 
 app.listen(8081, function () {
   console.log("listening on 8081");
 });
 
-// == 기본 셋팅
-
-// nodemon으로 서버 재실행 자동화하기 npm install -g nodemon
+/* 
+ == 기본 셋팅
+ nodemon으로 서버 재실행 자동화하기 npm install -g nodemon
+ */
 
 app.use("/public", express.static("public"));
 
 // css 사용
 
 app.get("/", function (요청, 응답) {
-  응답.sendFile(__dirname + "/index.html");
+  응답.sendFile(path.join(__dirname + "/", "views", "index.html"));
 });
 app.get("/login", function (요청, 응답) {
-  응답.sendFile(__dirname + "/login.html");
+  응답.sendFile(path.join(__dirname + "/", "views", "login.html"));
 });
 app.get("/MyPage", function (요청, 응답) {
-  응답.sendFile(__dirname + "/mypage.html");
+  응답.sendFile(path.join(__dirname + "/", "views", "mypage.html"));
 });
 app.get("/Review", function (요청, 응답) {
-  응답.sendFile(__dirname + "/review.html");
+  응답.sendFile(path.join(__dirname + "/", "views", "review.html"));
 });
